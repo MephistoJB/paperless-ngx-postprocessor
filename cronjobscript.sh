e8ae7eecd5171cf2b7fdcebd57a9b8dc3362a7d1
MephistoJB <6490305+MephistoJB@users.noreply.github.com>
2025-08-26 14:19:59 +0200

    Update cronjobscript.sh
    Path relativ

--- cronjobscript.sh
+++ cronjobscript.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ps -eaf | grep -i "paperlessngx_postprocessor.py" | grep -v grep
 # if not found - equals to 1, start it
 if [ $? -eq 1 ]
 then
 #echo "eq 0 - paperlessngx_postprocessor not running"
 printenv
-#/usr/src/paperless/paperless-ngx_postprocessor/venv/bin/python3 /usr/src/paperless/paperless-ngx_postprocessor/paperlessngx_postprocessor.py --verbose DEBUG process --tag Refresh
-/usr/src/paperless/paperless-ngx_postprocessor/venv/bin/python3 /usr/src/paperless/paperless-ngx_postprocessor/paperlessngx_postprocessor.py --verbose --tag Refresh
+#$DIR/venv/bin/python3 $DIR/paperlessngx_postprocessor.py --verbose DEBUG process --tag Refresh
+$DIR/venv/bin/python3 $DIR/paperlessngx_postprocessor.py --verbose --tag Refresh
 else
 echo "eq 0 - paperlessngx_postprocessor running - do nothing"
 fi
